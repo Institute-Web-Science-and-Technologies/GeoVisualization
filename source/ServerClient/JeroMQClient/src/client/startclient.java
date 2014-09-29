8f28421671cacc2a9060a0d1b7327becbd05932e
nilsgeilen <geilenn@uni-koblenz.de>
2014-09-29 15:53:13 +0200

    deleted historic code

--- source/ServerClient/JeroMQClient/src/client/startclient.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-public class startclient {
-	public static void main(String[] args) throws Exception {
-
-		new Thread(new Subscriber()).start();
-		ReqRepSubscriber.startClient();
-	}
-}
