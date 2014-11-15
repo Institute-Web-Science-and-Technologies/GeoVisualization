1f15126eb26a13f29d73af20bbc7d740954a4826
nilsgeilen <geilenn@uni-koblenz.de>
2014-11-15 16:48:55 +0100

    Revert "Revert "colliison logic""
    This reverts commit 022480a228a86b98524e503234e767fa2f9a3b60.

--- source/GUIPrototype/src/geoviz/game/Functions.java
+++ source/GUIPrototype/src/geoviz/game/Functions.java
@@ -108,16 +108,15 @@ public class Functions {
 		return false;
 	}
 	
-	public static boolean collides_simple(List<LatLng> list1, List<LatLng> list2) {
+	public static boolean collides_simple(List<LatLng> list1, List<LatLng> list2, double radius) {
 		if(list1.size()<1)
 			return false;
-		
-		double radius = 2;
-		
+				
 		LatLng head = list1.get(list1.size()-1);
 		for(LatLng l:list2){
-			double dist = Math.pow(head.latitude-l.latitude,2)+Math.pow(head.longitude-l.longitude,2);
-			if(dist<Math.pow(radius, 2))
+			//double dist = Math.pow(head.latitude-l.latitude,2)+Math.pow(head.longitude-l.longitude,2);
+			//if(dist<Math.pow(radius, 2))
+			if(distance(head,l)<radius)
 				return true;
 		}
 		return false;
