ccf8d54e710aec4e8872f17e03ad7d51a198c318
mothslayer <plambracht@uni-koblenz.de>
2014-11-18 15:16:36 +0100

    changed to online server

--- source/GUIPrototype/src/geoviz/communication/Const.java
+++ source/GUIPrototype/src/geoviz/communication/Const.java
@@ -2,7 +2,7 @@ package geoviz.communication;
 
 public class Const {
 	
-	//protected static 	final  String serverIP = "tcp://heglohitdos.west.uni-koblenz.de";
-	protected static final String serverIP ="tcp://192.168.2.108";
+	protected static 	final  String serverIP = "tcp://heglohitdos.west.uni-koblenz.de";
+	//protected static final String serverIP ="tcp://192.168.2.108";
 
 }
