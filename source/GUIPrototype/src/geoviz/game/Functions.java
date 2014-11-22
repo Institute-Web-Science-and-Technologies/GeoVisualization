6634ff222afebc1e3cc8b0b0f7ca8b8739bdc102
nilsgeilen <geilenn@uni-koblenz.de>
2014-11-22 19:52:25 +0100

    added comments

--- source/GUIPrototype/src/geoviz/game/Functions.java
+++ source/GUIPrototype/src/geoviz/game/Functions.java
@@ -9,6 +9,12 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class Functions {
 
+	/**
+	 * Calculates distance between two coordinates
+	 * @param d point
+	 * @param b point
+	 * @return distance from b to d
+	 */
 	public static float distance(LatLng d, LatLng b) {
 		Location l1 = new Location("From");
 		l1.setLatitude(d.latitude);
@@ -33,6 +39,13 @@ public class Functions {
 		 */
 	}
 
+	
+	/**
+	 * generates a random location 
+	 * @param ll 
+	 * @param radius
+	 * @return random location in a circle of radius radius around coordinate ll
+	 */
 	public static LatLng randLoc(LatLng ll, int radius) {
 		double y0 = ll.latitude, x0 = ll.longitude;
 		Random random = new Random();
@@ -56,6 +69,14 @@ public class Functions {
 		return res;
 	}
 	
+	
+	/**
+	 * tests if the line betwenn the last two points of list1 
+	 * intersects with any line between two points from list2
+	 * with indexes i and i+1.
+	 * @params lists of coordinates 
+	 * @return true iff collision occurred
+	 */
 	public static boolean collides(List<LatLng> list1, List<LatLng> list2) {
 		if ((list1.size() <= 1) || (list2.size() <= 1))
 			return false;
@@ -108,6 +129,12 @@ public class Functions {
 		return false;
 	}
 	
+	/**
+	 * tests wether the distance betwenn the first point of list1 
+	 * and any of the points of list2 is smaller than radius
+	 * @params lists of coordinates 
+	 * @return true iff collision occurred
+	 */
 	public static boolean collides_simple(List<LatLng> list1, List<LatLng> list2, double radius) {
 		if(list1.size()<1)
 			return false;
--- source/GUIPrototype/src/geoviz/game/snake/SnakeGame.java
+++ source/GUIPrototype/src/geoviz/game/snake/SnakeGame.java
@@ -17,10 +17,19 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class SnakeGame extends Game {
 
+	
+	//list of all players participating in this game 
 	private Map<String, Player> players = new HashMap<String, Player>();
 	private Map<String, Integer> highscore =new HashMap();
+	//list of all chickens
 	private List<Chicken> chickens = new LinkedList();
 
+	
+	/**
+	 * represents the game this phone takes part in
+	 * @param gameID the id of the game this phone takes part in
+	 * @param a interface stuff
+	 */
 	public SnakeGame(String gameID, SwipeScreen a) {
 		this.gameID = gameID;
 		this.swipeScreen = a;
@@ -31,20 +40,24 @@ public class SnakeGame extends Game {
 
 	}
 
+	
 	@Override
 	public void update(final TransferObject t) {
 		if (t.gameID.equals(this.gameID)) {
 			Player player = players.get(t.senderName);
+			//create a new player if no player with the sender's name exists
 			if (player == null) {
 				player = new Player(t.senderName);
 				players.put(t.senderName, player);
 
 			}
 			switch(t.msgType){
+			//adds a chciken created by sender 
 			case TransferObject.TYPE_ADD_CHICKEN:
 				chickens.add(new Chicken(t.pos, Const.CHICKEN_COLLISION_RADIUS, 1, t.msg));
 			break;
 			
+			//removes a chicken collected by sender
 			case TransferObject.TYPE_KILL_CHICKEN:
 				for (Chicken chicken : chickens) {
 					if (chicken.id.equals(t.msg))
@@ -54,6 +67,7 @@ public class SnakeGame extends Game {
 				players.get(t.senderName).changeMaxLength(Const.SNAKE_GROWTH_RATE);
 			break;
 			
+			//updates sender's player object with new inofrmation abut sender's physical position
 			case TransferObject.TYPE_COORD:
 				swipeScreen.runOnUiThread(new Runnable() {
 					public void run() {
@@ -88,6 +102,11 @@ public class SnakeGame extends Game {
 
 	}
 	
+	/**
+	 * add points th player's high score
+	 * @param player
+	 * @param point
+	 */
 	void addToHighscore(String player, int point){
 		if(highscore.containsKey(player))
 			highscore.put(player, highscore.get(player)+1);
@@ -95,6 +114,11 @@ public class SnakeGame extends Game {
 			highscore.put(player, 1);
 	}
 
+	/**
+	 * informs all phones taking part in the current game that chicken was created
+	 * and all information to create an equal chicken object
+	 * @param loc location where the chicken will be created
+	 */
 	public void addChicken(LatLng loc) {
 		final JeroMQQueue jmqq = JeroMQQueue.getInstance();
 		jmqq.sendMsg(TransferObject.TYPE_ADD_CHICKEN, loc,
