9aba425631454e04e752acd764ffe4c2893b7955
PhilippLambracht <plambracht@uni-koblenz.de>
2015-06-01 17:41:29 +0200

    Cooldown
    mit ClassCastException

--- source/GUIPrototype/gen/com/example/guiprototype/R.java
+++ source/GUIPrototype/gen/com/example/guiprototype/R.java
@@ -1491,7 +1491,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050036;
-        public static final int action_settings=0x7f050089;
+        public static final int action_settings=0x7f05008a;
         public static final int activity_chooser_view_content=0x7f050037;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1537,6 +1537,7 @@ containing a value of this type.
         public static final int fragmentLinearLayoutRightSide=0x7f05007b;
         public static final int fragmentLinearLayoutScore=0x7f050079;
         public static final int fragmentLinearLayoutShoot=0x7f050087;
+        public static final int fragmentMarkCooldown=0x7f050089;
         public static final int fragmentProgressBarPowUp=0x7f050084;
         public static final int fragmentProgressBlue=0x7f05007e;
         public static final int fragmentProgressRed=0x7f05007f;
